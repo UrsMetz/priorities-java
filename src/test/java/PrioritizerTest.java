4c24425c3bf6d2dc577d6b9bef91b10091fe7335
Urs Metz <urs.metz@gmx.de>
2013-06-14 21:44:23 +0200

    two items with different priority

--- src/test/java/PrioritizerTest.java
+++ src/test/java/PrioritizerTest.java
@@ -5,10 +5,14 @@ import java.util.Collections;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class PrioritizerTest {
+
+    private static final String NAME_IGNORED = "name";
+
     @Test
     public void emptyListOfItemShouldYieldEmptyResult() {
         assertThat(prioritize(Collections.<PriorityItem>emptyList()), is(empty()));
@@ -16,11 +20,25 @@ public class PrioritizerTest {
 
     @Test
     public void listOfOneItemShouldYieldThatItem() {
-        PriorityItem item = new PriorityItem("bla", 1);
+        PriorityItem item = new PriorityItem(NAME_IGNORED, 1);
         assertThat(prioritize(asList(item)), contains(item));
     }
 
+    @Test
+    public void listOfTwoItemsWithDifferentPrioritiesShouldYieldTheHigherPrioritized() {
+        PriorityItem lowerPrioritized = new PriorityItem(NAME_IGNORED, 1);
+        PriorityItem higherPrioritized = new PriorityItem(NAME_IGNORED, 2);
+
+        assertThat(prioritize(asList(lowerPrioritized, higherPrioritized)), contains(higherPrioritized));
+    }
+
     private List<PriorityItem> prioritize(List<PriorityItem> items) {
-        return items;
+        if (items.isEmpty())
+            return emptyList();
+        return lastElementAsList(items);
+    }
+
+    private List<PriorityItem> lastElementAsList(List<PriorityItem> items) {
+        return asList(items.get(items.size() - 1));
     }
 }
