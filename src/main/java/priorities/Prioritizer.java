f8f8dd52aebf19bc1625e662906cf37abc58f780
Urs Metz <urs.metz@gmx.de>
2013-06-14 22:44:00 +0200

    move production code into its own class

---
+++ src/main/java/priorities/Prioritizer.java
@@ -0,0 +1,47 @@
+package priorities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class Prioritizer {
+    private final List<PriorityItem> items;
+
+    public Prioritizer(List<PriorityItem> items) {
+        this.items = items;
+    }
+
+    public List<PriorityItem> proritize() {
+        if (items.isEmpty())
+            return emptyList();
+        final int maxPriority = findMaxPriority(items);
+
+        return findAllWithPriority(items, maxPriority);
+    }
+
+    private List<PriorityItem> findAllWithPriority(List<PriorityItem> items, int priority) {
+        List<PriorityItem> allWithGivenPriority = new ArrayList<PriorityItem>();
+        for (PriorityItem item : items) {
+            if (item.getPriority() == priority) {
+                allWithGivenPriority.add(item);
+            }
+        }
+        return allWithGivenPriority;
+    }
+
+    private int findMaxPriority(List<PriorityItem> items) {
+        return Collections.max(items, compareByPriority()).getPriority();
+    }
+
+    private Comparator<PriorityItem> compareByPriority() {
+        return new Comparator<PriorityItem>() {
+            @Override
+            public int compare(PriorityItem o1, PriorityItem o2) {
+                return o1.getPriority() - o2.getPriority();
+            }
+        };
+    }
+}
--- src/test/java/PrioritizerTest.java
+++ src/test/java/PrioritizerTest.java
@@ -1,13 +1,10 @@
 import org.junit.Test;
+import priorities.Prioritizer;
 import priorities.PriorityItem;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -17,13 +14,13 @@ public class PrioritizerTest {
 
     @Test
     public void emptyListOfItemShouldYieldEmptyResult() {
-        assertThat(prioritize(Collections.<PriorityItem>emptyList()), is(empty()));
+        assertThat(new Prioritizer(Collections.<PriorityItem>emptyList()).proritize(), is(empty()));
     }
 
     @Test
     public void listOfOneItemShouldYieldThatItem() {
         PriorityItem item = new PriorityItem(NAME_IGNORED, 1);
-        assertThat(prioritize(asList(item)), contains(item));
+        assertThat(new Prioritizer(asList(item)).proritize(), contains(item));
     }
 
     @Test
@@ -31,7 +28,7 @@ public class PrioritizerTest {
         PriorityItem lowerPrioritized = new PriorityItem(NAME_IGNORED + "1", 1);
         PriorityItem higherPrioritized = new PriorityItem(NAME_IGNORED + "2", 2);
 
-        assertThat(prioritize(asList(lowerPrioritized, higherPrioritized)), contains(higherPrioritized));
+        assertThat(new Prioritizer(asList(lowerPrioritized, higherPrioritized)).proritize(), contains(higherPrioritized));
     }
 
     @Test
@@ -39,38 +36,7 @@ public class PrioritizerTest {
         PriorityItem item1 = new PriorityItem("first item", 1);
         PriorityItem item2 = new PriorityItem("second item" + "2", 1);
 
-        assertThat(prioritize(asList(item1, item2)), containsInAnyOrder(item1, item2));
-    }
-
-    private List<PriorityItem> prioritize(List<PriorityItem> items) {
-        if (items.isEmpty())
-            return emptyList();
-        final int maxPriority = findMaxPriority(items);
-
-        return findAllWithPriority(items, maxPriority);
-    }
-
-    private List<PriorityItem> findAllWithPriority(List<PriorityItem> items, int priority) {
-        List<PriorityItem> allWithGivenPriority = new ArrayList<PriorityItem>();
-        for (PriorityItem item : items) {
-            if (item.getPriority() == priority) {
-                allWithGivenPriority.add(item);
-            }
-        }
-        return allWithGivenPriority;
-    }
-
-    private int findMaxPriority(List<PriorityItem> items) {
-        return Collections.max(items, compareByPriority()).getPriority();
-    }
-
-    private Comparator<PriorityItem> compareByPriority() {
-        return new Comparator<PriorityItem>() {
-            @Override
-            public int compare(PriorityItem o1, PriorityItem o2) {
-                return o1.getPriority() - o2.getPriority();
-            }
-        };
+        assertThat(new Prioritizer(asList(item1, item2)).proritize(), containsInAnyOrder(item1, item2));
     }
 
 }
